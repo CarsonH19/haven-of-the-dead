e15a6b24b3363ddae1b145379cc86e1bae697aca
CarsonH19 <questline.carson@gmail.com>
2023-10-21 23:55:11 +0900

    fix: variables moved to globalVariables.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,74 +1,4 @@
-// ===============================
-//        GeneralVariables
-// ===============================
 
-const monsterHealthBar = document.getElementById("monster-health");
-const playerHealthBar = document.getElementById("player-health");
-
-const attackBtn = document.getElementById("attack-btn");
-const guardBtn = document.getElementById("guard-btn");
-const specialBtn = document.getElementById("special-btn");
-const potionBtn = document.getElementById("potion-btn");
-const fleeBtn = document.getElementById("flee-btn");
-
-let monsterMaxHealth = 100;
-let currentMonsterHealth = monsterMaxHealth;
-let monsterAttackValue = 10;
-
-let currentPlayerHealth;
-
-// ===============================
-//        Hero Variables
-// ===============================
-let heroChoice;
-let baseAttack;
-let baseHealth;
-let specialCooldownCounter = 0;
-
-// Strength
-let baseStrength;
-let strengthBonusHealth;
-let playerMaxHealth = baseHealth + strengthBonusHealth;
-
-function calculateStrengthBonusHealth() {
-  return baseStrength * 10;
-}
-
-function calculatePlayerMaxHealth() {
-  return baseHealth + strengthBonusHealth;
-}
-
-// Dexterity
-let baseDexterity;
-let dexterityCritIncrease = calculateDexCritIncrease();
-let baseCritModifier = 1.5 + calculateBaseCritModifier();
-let criticalDamage;
-
-function calculateDexCritIncrease() {
-  return 0.3 * baseDexterity;
-}
-
-function calculateBaseCritModifier() {
-  return 1.5 + dexterityCritIncrease;
-}
-
-function calculateCritDamage() {
-  dexterityCritIncrease = calculateDexCritIncrease();
-  baseCritModifier = calculateBaseCritModifier();
-  console.log("baseAttack:", baseAttack);
-  console.log("dexterityCritIncrease:", dexterityCritIncrease);
-  console.log("baseCritModifier:", baseCritModifier);
-  console.log(Math.round(baseAttack * baseCritModifier));
-  return Math.round(baseAttack * baseCritModifier);
-}
-
-// Faith
-let baseFaith; // add faith to flee function after random number
-let findItemChance; // use when items are finished
-
-// Special & Abilities
-let specialAbility;
-let passiveAbility;
 
 // ===============================
 //             Health
@@ -395,6 +325,47 @@ let availableBoons = [];
 //       Monster Stat Block
 // ===============================
 
+function monsterSkullLevel(level) {
+  switch (level) {
+    case 1:
+    monsterMaxHealth = 20;
+    monsterAttackValue = 3;
+    break;
+  case 2:
+    monsterMaxHealth = 30;
+    monsterAttackValue = 4;
+    break;
+  case 3:
+    monsterMaxHealth = 40;
+    monsterAttackValue = 5;
+    break;
+  case 4:
+    monsterMaxHealth = 50;
+    monsterAttackValue = 6;
+    break;
+  case 5:
+    monsterMaxHealth = 70;
+    monsterAttackValue = 8;
+    break;
+  case 6:
+    monsterMaxHealth = 90;
+    monsterAttackValue = 10;
+    break;
+  case 7:
+    monsterMaxHealth =110;
+    monsterAttackValue = 12;
+    break;
+  case 8:
+    monsterMaxHealth = 150;
+    monsterAttackValue = 15;
+    break;
+  case 9:
+    monsterMaxHealth = 200;
+    monsterAttackValue = 18
+    break;
+}
+  }
+
 function renderMonsterStatBlock(monster) {
 
 }
@@ -420,16 +391,17 @@ window.addEventListener("click", function (event) {
     if (event.target === siggurd) {
       heroChoice = "paladin";
       setPaladinStats();
+      getRandomRoom(catacombRooms);
     } else if (event.target === riven) {
       heroChoice = "rogue";
       setRogueStats();
+      getRandomRoom(catacombRooms);
     } else if (event.target === liheth) {
       heroChoice = "priestess";
       setPriestessStats();
+      getRandomRoom(catacombRooms);
     }
   }
-  
-  getRandomRoom(catacombRooms);
 });
 
 adjustMonsterHealth(monsterMaxHealth);
